4fc2780941a03bef715f1f9a8e1a8235a05b3a92
varun <varunnidumm@gmail.com>
2023-11-28 15:52:36 +0530

    adjust image size

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function displayData(cartGroup) {
     for (let product of cart.products) {
       const htmlData = `
             <div class="card m-2" style="width: 18rem">
-             <img src="${product.thumbnail}" class="card-img-top" alt="..." />
+             <img src="${product.thumbnail}" class="card-img-top" alt="..." style="height:286px"/>
               <div class="card-body">
                <h5 class="card-title">${product.title}</h5>
                <p class="card-text">price : ${product.discountedPrice}</p>
